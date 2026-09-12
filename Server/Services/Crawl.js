3ea8d682ca35a5cd8cd54f81fa0d889a80e5bba9
VAMSHIYADAV46 <nanidx46@gmail.com>
2026-09-12 21:49:06 +0530

    Fix crawler queue duplicate handling

--- Server/Services/Crawl.js
+++ Server/Services/Crawl.js
@@ -4,19 +4,24 @@ import { popFirstFromSet } from "../utilities.js";
 
 export async function Crawl({ url, maxDepth, sameDomain }) {
   let visited = new Set();
-  let queue = new Set();
-  queue.add(url);
+  let queue = new Map();
+
+  queue.set(url, 0);
 
   let result = [];
 
   while (queue.size != 0) {
     try {
-      let currentUrl = popFirstFromSet(queue);
+      let [currentUrl, currentDepth] = queue.entries().next().value;
+      queue.delete(currentUrl);
+
       if (!currentUrl) {
         throw new Error("Invalid Url");
       }
-      if (!visited.has(currentUrl)) {
-        console.log("Crawling:", currentUrl);
+
+      if (!visited.has(currentUrl) && currentDepth <= maxDepth) {
+        console.log("Crawling:", currentUrl, "Depth:", currentDepth);
+
         const response = await axios.get(currentUrl, {
           headers: {
             "User-Agent":
@@ -27,26 +32,47 @@ export async function Crawl({ url, maxDepth, sameDomain }) {
         visited.add(currentUrl);
 
         const html = response.data;
-
         const $ = cheerio.load(html);
 
         let uniqueUrls = new Set();
 
         let title = $("title").text().trim();
+
         $("a").each((ind, element) => {
           let link = $(element).attr("href");
+
           if (link) {
             let checkedUrl = new URL(link, currentUrl);
+
             if (
-              checkedUrl.protocol == "https:" ||
-              checkedUrl.protocol == "http:"
+              (checkedUrl.protocol == "https:" ||
+                checkedUrl.protocol == "http:") &&
+              currentDepth < maxDepth
             ) {
               uniqueUrls.add(checkedUrl.href);
-              queue.add(checkedUrl.href);
+
+              if (
+                !visited.has(checkedUrl.href) &&
+                !queue.has(checkedUrl.href)
+              ) {
+                queue.set(checkedUrl.href, currentDepth + 1);
+
+                console.log(
+                  "Adding:",
+                  checkedUrl.href,
+                  "Depth:",
+                  currentDepth + 1
+                );
+              }
             }
           }
         });
-        result.push({ url: currentUrl, title: title, urls: [...uniqueUrls] });
+
+        result.push({
+          url: currentUrl,
+          title: title,
+          urls: [...uniqueUrls],
+        });
       }
     } catch (error) {
       throw new Error(`Caught error while crawling : ${error.message}`);
