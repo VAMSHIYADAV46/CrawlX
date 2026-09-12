a270b54bb402953f5380119c710cf315076ec04b
VAMSHIYADAV46 <nanidx46@gmail.com>
2026-09-12 10:17:51 +0530

    Add visited URL tracking

--- Server/Controllers/crawlController.js
+++ Server/Controllers/crawlController.js
@@ -3,7 +3,7 @@ import * as Services from '../Services/Crawl.js'
 export async function startCrawl(req,res) {
     let {url,maxDepth,sameDomain} = req.body
 
-    let resp = await Services.Crawl(url,maxDepth,sameDomain)
+    let resp = await Services.Crawl({url,maxDepth,sameDomain})
     
     res.json(resp)
 }
--- Server/Services/Crawl.js
+++ Server/Services/Crawl.js
@@ -1,44 +1,45 @@
-import * as cheerio from 'cheerio'
-import axios from 'axios'
+import * as cheerio from "cheerio";
+import axios from "axios";
 
-export async function Crawl(url){
+export async function Crawl({ url, maxDepth, sameDomain }) {
+  let visited = new Set();
 
-try {
-    if(!url){
-        throw new Error("Invalid Url")
+  try {
+    if (!url) {
+      throw new Error("Invalid Url");
     }
-    const response = await axios.get(url, {
-      headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/140.0.0.0 Safari/537.36",
-      },
-    });
+    if (!visited.has(url)) {
+      const response = await axios.get(url, {
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/140.0.0.0 Safari/537.36",
+        },
+      });
+      visited.add(url);
 
-    const html = response.data
+      const html = response.data;
 
-    const $ = cheerio.load(html)
+      const $ = cheerio.load(html);
 
-    let uniqueUrls = new Set()
+      let uniqueUrls = new Set();
 
-    let title = $('title').text().trim()
-    $('a').each((ind,element) => {
-      let link = $(element).attr('href')
-      if(link){
-        let checkedUrl = new URL(link,url)
-        uniqueUrls.add(checkedUrl.href)
-      }
-    });
-
-
-    return {
-      success: true,
-      url: url,
-      title: title,
-      uniqueUrls : uniqueUrls
-    };
-} catch (error) {
-    throw new Error(`Caught error while crawling : ${error.message}`)
-}
+      let title = $("title").text().trim();
+      $("a").each((ind, element) => {
+        let link = $(element).attr("href");
+        if (link) {
+          let checkedUrl = new URL(link, url);
+          uniqueUrls.add(checkedUrl.href);
+        }
+      });
+    }
+  } catch (error) {
+    throw new Error(`Caught error while crawling : ${error.message}`);
+  }
 }
 
-
+// return {
+//   success: true,
+//   url: url,
+//   title: title,
+//   uniqueUrls: [...uniqueUrls]
+// };
